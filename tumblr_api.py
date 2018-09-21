7fc8b1ca7f3fe7f826d194669e2919a89e4f6de5
Peixuan Liu <Olivia@MacBook-Pro-4.local>
2018-09-21 13:00:09 -0700

    added export and data structure interface to tumblr

--- tumblr_api.py
+++ tumblr_api.py
@@ -72,3 +72,165 @@ class Tumblr:
             options=None
     ):
         pass # TODO: export specified data to
+
+
+class Blog (BareboneNode):
+    def __init__(
+            self,
+            title,
+            hostname,
+            num_posts,
+            last_time_updated,
+            description,
+            num_likes
+    ):
+        self.title = title
+        self.hostname = hostname
+        self.num_posts = num_posts
+        self.last_time_updated = last_time_updated
+        self.description = description
+        self.num_likes = num_likes
+
+
+class Post (BareboneNode):
+    def __init__(
+            self,
+            type,
+            timestamp,
+            date,
+            tags,
+            limit,
+            state,
+            liked,
+            num_posts,
+            creator
+    ):
+        self.type = type
+        self.timestamp = timestamp
+        self.date = date
+        self.tags = tags
+        self.limit = limit
+        self.state  = state
+        self.liked = liked
+        self.limit = limit
+        self.state = state
+        self.liked = liked
+        self.num_posts = num_posts
+        self.creator = creator
+        #TODO: add methods
+
+
+class TextPost(Post):
+    def __init__(
+            self,
+            type,
+            timestamp,
+            date,
+            tags,
+            limit,
+            state,
+            liked,
+            num_posts,
+            creator,
+            title,
+            body
+    ):
+        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
+        self.title = title
+        self.body = body
+
+
+class PhotoPost(Post):
+    def __init__(
+            self,
+            type,
+            timestamp,
+            date,
+            tags,
+            limit,
+            state,
+            liked,
+            num_posts,
+            creator,
+            photos,
+            caption,
+            width,
+            height
+    ):
+        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
+        self.photos = photos
+        self.caption = caption
+        self.width = width
+        self.height = height
+
+
+class QuotePost(Post):
+    def __init__(
+            self,
+            type,
+            timestamp,
+            date,
+            tags,
+            limit,
+            state,
+            liked,
+            num_posts,
+            creator,
+            text,
+            source
+    ):
+        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
+        self.text = text
+        self.source = source
+
+
+class LinkPost(Post):
+    def __init__(
+            self,
+            type,
+            timestamp,
+            date,
+            tags,
+            limit,
+            state,
+            liked,
+            num_posts,
+            creator,
+            title,
+            url,
+            author,
+            excerpt,
+            publisher,
+            photos,
+            description
+    ):
+        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
+        self.title = title
+        self.url = url
+        self.author = author
+        self.excerpt = excerpt
+        self.publisher = publisher
+        self.photos = photos
+        self.description = description
+
+
+# TODO: continue with chat, audio, video and answer posts
+
+
+# TODO: continue with edges
+class Like_post(BareboneEdge):
+    def __init__(
+            self,
+            user_id,
+            post_id
+    ):
+        BareboneEdge.__init__(self, user_id, post_id)
+
+
+class Follow_blog(BareboneEdge):
+    def __init__(
+            self,
+            following_blog_id,
+            followed_blog_id
+    ):
+        BareboneEdge.__init__(self, following_blog_id, followed_blog_id)
