88dbe31ebaacef0f4d7e5a784c9c255a6ecfdb1c
zifanshi <zifanshi@usc.edu>
2018-09-24 00:40:33 -0700

    modify youtube_api to pandas for fetching videos (minor change as Kangkang requested)

--- youtube_api.py
+++ youtube_api.py
@@ -3,6 +3,7 @@
 import httplib2
 import os
 import sys
+import pandas as pd
 
 from apiclient.discovery import build_from_document
 from oauth2client.client import flow_from_clientsecrets
@@ -139,6 +140,16 @@ class Comment:
         self.author_display_name = comment['snippet']['authorDisplayName']
 
 
+def parse_comment_to_node(comment):
+    return {'updated_at': comment['snippet']['updatedAt'],
+            'published_at': comment['snippet']['publishedAt'],
+            'view_rating': comment['snippet']['viewerRating'],
+            'can_rate': comment['snippet']['canRate'],
+            'text_original': comment['snippet']['textOriginal'],
+            'text_display': comment['snippet']['textDisplay'],
+            'like_count': comment['snippet']['likeCount']}
+
+
 class Youtube:
     def __init__(self, api):
         self.YOUTUBE_READ_WRITE_SSL_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
@@ -167,9 +178,10 @@ class Youtube:
         self.video_comments = []
         self.youtube = build(self.YOUTUBE_API_SERVICE_NAME, self.YOUTUBE_API_VERSION,
                              developerKey=self.DEVELOPER_KEY)
+        self.get_authenticated_service()
 
     # Authorize the request and store authorization credentials.
-    def get_authenticated_service(self, args):
+    def get_authenticated_service(self):
         flow = flow_from_clientsecrets(self.CLIENT_SECRET_FILE, scope=self.YOUTUBE_READ_WRITE_SSL_SCOPE,
                                        message=self.MISSING_CLIENT_SECRET_MESSAGE)
 
@@ -177,7 +189,7 @@ class Youtube:
         credentials = storage.get()
 
         if credentials is None or credentials.invalid:
-            credentials = run_flow(flow, storage, args)
+            credentials = run_flow(flow, storage)
 
         # Trusted testers can download this discovery document from the developers page
         # and it should be in the same directory with the code.
@@ -185,7 +197,68 @@ class Youtube:
             doc = f.read()
             return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
 
-    # Call the API's commentThreads.list method to list the existing comments.
+    def fetch_video(self, options):
+        search_response = self.youtube.search().list(
+            q=options.q,
+            part='id,snippet',
+            maxResults=options.max_results
+        ).execute()
+
+        video_nodes = []
+        video_edges = []
+        channel_nodes = []
+        # video_comments = []
+
+        try:
+            for search_result in search_response.get('items', []):
+                if search_result['id']['kind'] == 'youtube#video':
+                    video_id = search_result['id']['videoId']
+                    video_node = {'published_at': search_result['snippet']['publishedAt'],
+                                  'video_id': search_result['id']['videoId'],
+                                  'title': search_result['snippet']['title'],
+                                  'description': search_result['snippet']['description']}
+                    video_nodes.append(video_node)
+
+                    video_edges.append({'Source': search_result['snippet']['channelId'],
+                                        'Target': video_id})
+
+                    channel_nodes.append({'channel_id': search_result['snippet']['channelId'],
+                                          'channel_title': search_result['snippet']['channelTitle']})
+
+                    # video_comments.append(self.fetch_video_comment(video_id))
+
+        except HttpError as e:
+            print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+
+        video_nodes_df = pd.DataFrame(video_nodes)
+        video_edges_df = pd.DataFrame(video_edges)
+        channel_nodes_df = pd.DataFrame(channel_nodes)
+        video_nodes_df.to_csv("1.csv", encoding='UTF-8', index=False)
+        video_edges_df.to_csv("2.csv", encoding='UTF-8', index=False)
+        channel_nodes_df.to_csv("3.csv", encoding='UTF-8', index=False)
+        print video_nodes_df
+        print video_edges_df
+        print channel_nodes_df
+
+    def fetch_video_comment(self, video_id):
+        comment_nodes = []
+        comment_user_edges = []
+        comment_reply_edges = []
+        comment_threads = get_comments(self.youtube, video_id, None)
+        for comment_thread in comment_threads:
+            top_level_comment = comment_thread['snippet']['topLevelComment']
+
+            comment_user_edge = {'comment_id': top_level_comment['id'],
+                                 'channel_id': comment_thread['snippet']['authorChannelId']['value'],
+                                 'channel_url': comment_thread['snippet']['authorChannelUrl'],
+                                 'author_display_name': comment_thread['snippet']['authorDisplayName']}
+            comment_user_edges.append(comment_user_edge)
+
+            top_level_comment_node = parse_comment_to_node(top_level_comment)
+            comment_nodes.append(top_level_comment_node)
+
+            replies = top_level_comment['replies']['comments']
+        pass
 
     def youtube_search(self, options):
 
@@ -210,7 +283,7 @@ class Youtube:
                         comment = Comment(topLevelComment)
                         comment.videoId = search_result['id']['videoId']
                         self.video_comments.append(comment)
-                    
+
 
                 elif search_result['id']['kind'] == 'youtube#channel':
                     self.channels.append(Channel(search_result))
@@ -248,34 +321,34 @@ class Youtube:
         for video in self.videos:
             print("\tvideoId: " + (video.video_id).encode("utf-8"))
             print("\ttitle: " + (video.title).encode("utf-8"))
-            print("\tdescription: "+ (video.description).encode("utf-8"))
-            print("\tpublished at: "+ (video.published_at).encode("utf-8"))
+            print("\tdescription: " + (video.description).encode("utf-8"))
+            print("\tpublished at: " + (video.published_at).encode("utf-8"))
             print
 
         print('Channels:')
         for channel in self.channels:
-            print("\tchannelId: "+ (channel.channel_id).encode("utf-8"))
-            print("\ttitle: "+ (channel.channel_title).encode("utf-8"))
-            print("\tdescription: "+  (channel.description).encode("utf-8"))
+            print("\tchannelId: " + (channel.channel_id).encode("utf-8"))
+            print("\ttitle: " + (channel.channel_title).encode("utf-8"))
+            print("\tdescription: " + (channel.description).encode("utf-8"))
             print
 
         print('Playlist:')
         for playlist in self.playlists:
-            print("\tplaylistId: "+  (playlist.playlist_id).encode("utf-8"))
-            print("\ttitle: "+  (playlist.title).encode("utf-8"))
-            print("\tdescription: "+  (playlist.description).encode("utf-8"))
-            print("\tpublished at: "+  (playlist.published_at).encode("utf-8"))
+            print("\tplaylistId: " + (playlist.playlist_id).encode("utf-8"))
+            print("\ttitle: " + (playlist.title).encode("utf-8"))
+            print("\tdescription: " + (playlist.description).encode("utf-8"))
+            print("\tpublished at: " + (playlist.published_at).encode("utf-8"))
             print
 
-        print('Comments:\n')
-        for comment in self.video_comments:
-            if comment.video_id is not None:
-                print("\tvideoId: " + (comment.video_id).encode("utf-8"))
-            print("\tupdated_at: " + (comment.updated_at).encode("utf-8"))
-            print("\tpublished_at: " + (comment.published_at).encode("utf-8"))
-            print("\tview rating: " + (comment.view_rating).encode("utf-8"))
-            print("\tcan rate: " + str(comment.can_rate))
-            print("\ttext_original: " + (comment.text_original).encode("utf-8"))
-            print("\ttext_display: " + (comment.text_display).encode("utf-8"))
-            print("\tlike count: " + str(comment.like_count))
-            print
+            # print('Comments:\n')
+            # for comment in self.video_comments:
+            #     if comment.video_id is not None:
+            #         print("\tvideoId: " + (comment.video_id).encode("utf-8"))
+            #     print("\tupdated_at: " + (comment.updated_at).encode("utf-8"))
+            #     print("\tpublished_at: " + (comment.published_at).encode("utf-8"))
+            #     print("\tview rating: " + (comment.view_rating).encode("utf-8"))
+            #     print("\tcan rate: " + str(comment.can_rate))
+            #     print("\ttext_original: " + (comment.text_original).encode("utf-8"))
+            #     print("\ttext_display: " + (comment.text_display).encode("utf-8"))
+            #     print("\tlike count: " + str(comment.like_count))
+            #     print
