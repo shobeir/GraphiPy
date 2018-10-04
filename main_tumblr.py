61de5b517df565305f2b7bf66a3fada944396cbb
Peixuan Liu <Olivia@MacBook-Pro-4.local>
2018-10-04 16:30:30 -0700

    tumblr csv files are exported

--- main_tumblr.py
+++ main_tumblr.py
@@ -1,4 +1,5 @@
 from tumblr_api import Tumblr
+import pandas as pd
 
 
 class USDE:
@@ -9,19 +10,51 @@ class USDE:
     def get_tumblr(self):
         return self.tumblr
 
+    def exportCSV(self, dataframes, prefix):
+        for key in dataframes.keys():
+            dataframes[key].to_csv(
+                prefix + "_" + key + ".csv", encoding="utf-8", index=False)
 
-api = {
-    "tumblr": {
-        "consumer_key": ' ',
-        "consumer_secret": ' ',
-        "oauth_token": ' ',
-        "oauth_secret": ' '
+    def merge(self, main_df, target_df):
+        for key in main_df.keys():
+            main_df[key] = main_df[key].append(target_df[key])
+
+    def remove_duplicates(self, df):
+        for key in df.keys():
+            if df[key].empty:
+                continue
+            if {"id"}.issubset(df[key].columns):
+                df[key].drop_duplicates(subset=["id"], inplace=True)
+            else:
+                df[key].drop_duplicates(inplace=True)
+
+    def get_df_from_list(self, df_list):
+        temp_df = {}
+        for key in df_list:
+            temp_df[key] = None
+
+        for key in temp_df.keys():
+            temp_df[key] = pd.concat(df_list[key])
+
+        return temp_df
+
+
+def main():
+    api = {
+        "tumblr": {
+            "consumer_key": ' ',
+            "consumer_secret": ' ',
+            "oauth_token": ' ',
+            "oauth_secret": ' '
+        }
     }
-}
 
-usde = USDE(api)
-tumblr = usde.get_tumblr()
-#tumblr.fetch_tumblr_posts_tagged(tag='trump')
-#tumblr.fetch_tumblr_blog_following('azspot')
-#tumblr.fetch_tumblr_blog_followers('azspot')
-tumblr.fetch_tumblr_posts_published('azspot')
+    usde = USDE(api)
+    tumblr = usde.get_tumblr()
+
+
+    tumblr.fetch_tumblr_blog(blog_name="azspot")
+  #  df_tag = tumblr.fetch_tumblr_posts_tagged(tag="overwatch")
+
+if __name__ == '__main__':
+    main()
--- tumblr_api.py
+++ tumblr_api.py
@@ -1,17 +1,18 @@
 import pytumblr
 import pandas as pd
 
+
 def create_blog_node(blog):
     blog_node = {
-        "id": blog.id,  # ???
-        "name": blog.name,
-        "title": blog.title,
-        "posts": blog.posts,  # Number num_posts
-        "updated": blog.updated, # Number The time of the most recent post, in seconds since the epoch
-        "description": blog.description,
+        #"id": blog['id'],  # ???
+        "name": blog['name'],
+        "title": blog['title'],
+        #"posts": blog['posts'],  # Number num_posts
+        "updated": blog['updated'], # Number The time of the most recent post, in seconds since the epoch
+        "description": blog['description'],
         # "ask": blog.ask, # Boolean Indicates whether the blog allows questions
         # "ask_anon": blog.ask_anon,  # Boolean Indicates whether the blog allows anonymous questions
-        "likes": blog.likes,  # Number Number of likes for this use
+        # "likes": blog['likes'],  # Number Number of likes for this use
         # "is_blocked_from_primary": blog.is_blocked_from_primary
         # Boolean Indicates whether this blog has been blocked by the calling user's primary blog
     }
@@ -20,27 +21,27 @@ def create_blog_node(blog):
 
 def create_post_node(post):
     post_node = {
-        "id": post.id,  # ???
-        "blog_name": post.blog_name,
-        "post_url": post.post_url,
-        "type": post.type,
-        "timestamp": post.timestamp,  # Number The time of the post, in seconds since the epoch
-        "date": post.date,  # The GMT date and time of the post, as a string
-        "format": post.format,  # String The post format: html or markdown
+        "id": post['id'],  # ???
+        "blog_name": post['blog_name'],
+        "post_url": post['post_url'],
+        "type": post['type'],
+        "timestamp": post['timestamp'],  # Number The time of the post, in seconds since the epoch
+        "date": post['date'],  # The GMT date and time of the post, as a string
+        "format": post['format'],  # String The post format: html or markdown
         # "reblog_key": post.reblog_key,  # String The key used to reblog this post
-        "tags": post.tags,  # Array(string) Tags applied to the post
+        "tags": post['tags'],  # Array(string) Tags applied to the post
         # "bookmarklet": post.bookmarklet,  # Boolean Indicates whether the post was created via the Tumblr bookmarklet
         # "mobile": post.mobile,  # Boolean Indicates whether the post was created via mobile / email publishing
-        "source_url": post.source_url,  # String The URL for the source of the content ( for quotes, reblogs, etc.)
-        "source_title": post.source_title,
+        # "source_url": post['source_url'],  # String The URL for the source of the content ( for quotes, reblogs, etc.)
+        # "source_title": post['source_title'],
         # "liked": post.liked,
-        "state": post.state,
-        "total_posts": post.total_posts
+        "state": post['state'],
+        # "total_posts": post['total_posts']
     }
     return post_node
 
 
-# Typed post nodes are coming
+# Typed post nodes are coming...
 
 def create_edge(source, target):
     edge = {
@@ -49,10 +50,6 @@ def create_edge(source, target):
         "Type": "undirected"
     }
     return edge
-
-
-def test(self):
-    pass
 
 
 class Tumblr:
@@ -64,19 +61,44 @@ class Tumblr:
             oauth_secret=api["oauth_secret"]
         )
 
+    def exportCSV(self, dataframes, prefix):
+
+        for key in dataframes.keys():
+            dataframes[key].to_csv(
+                prefix + "_" + key + ".csv", encoding="utf-8", index=False)
+
+    def fetch_tumblr_blog(
+            self,
+            blog_name
+    ):
+        dataframes_info = self.fetch_tumblr_blog_info(blog_name)
+        self.exportCSV(dataframes_info, "tumblr")
+        dataframes_followed_bogs = self.fetch_tumblr_followed_blogs(blog_name)
+        self.exportCSV(dataframes_followed_bogs, "tumblr")
+        dataframes_published_posts = self.fetch_tumblr_published_posts(blog_name)
+        self.exportCSV(dataframes_published_posts, "tumblr")
+        dataframes_liked_posts = self.fetch_tumblr_liked_posts(blog_name)
+        self.exportCSV(dataframes_liked_posts, "tumblr")
+        return
+
     def fetch_tumblr_blog_info(
         self,
         blog_name
     ):
-        return self.tumblr.blog_info(blog_name)
+        blogs_list = []
+        blogs_list.append(create_blog_node(self.tumblr.blog_info(blog_name)['blog']))
+        dataframes = {
+            "blogs": pd.DataFrame(blogs_list)
+        }
+        return dataframes
 
-    def fetch_tumblr_blog_following(
+    def fetch_tumblr_followed_blogs(
         self,
         blog_name,
         limit=20,
         offset=0
     ):
-        blog_node_self = create_blog_node(self.fetch_tumblr_blog_info(blog_name))
+        blog_node_self = create_blog_node(self.tumblr.blog_info(blog_name)['blog'])
         blogs_following = self.tumblr.blog_following(blog_name)['blogs']
 
         blogs_list = []
@@ -85,9 +107,9 @@ class Tumblr:
         blogs_list.append(blog_node_self)
 
         for blog in blogs_following:
-            blog_node = create_blog_node(blog.name)
+            blog_node = create_blog_node(blog)
             blogs_list.append(blog_node)
-            follow_blog_edges.append(create_edge(blog_node_self.id, blog_node.id))
+            follow_blog_edges.append(create_edge(blog_node_self['name'], blog_node['name']))
 
         dataframes = {
             "blogs": pd.DataFrame(blogs_list),
@@ -114,7 +136,7 @@ class Tumblr:
         }
         return dataframes"""
 
-    def fetch_tumblr_posts_published(
+    def fetch_tumblr_published_posts(
         self,
         blog_name,  # blog name
         type="text",
@@ -126,16 +148,20 @@ class Tumblr:
         posts = self.tumblr.posts(blog_name)['posts']
 
         posts_list = []
+        publish_post_edges = []
 
         for post in posts:
-            posts.append(create_post_node(post))
+            post_node = create_post_node(post)
+            posts_list.append(post_node)
+            publish_post_edges.append(create_edge(blog_name, post_node['id']))
 
         dataframes = {
-            "posts": pd.DataFrame(posts_list)
+            "posts": pd.DataFrame(posts_list),
+            "publish_post_edges": pd.DataFrame(publish_post_edges)
         }
         return dataframes
 
-    def fetch_tumblr_posts_liked(
+    def fetch_tumblr_liked_posts(
         self,
         blog_name,
         limit=20,
@@ -143,17 +169,15 @@ class Tumblr:
         before=0,
         after=0
     ):
-        posts = self.tumblr.blog_likes(blog_name)['posts']
-
-        blog_node = create_blog_node(blog_name)
+        posts = self.tumblr.blog_likes(blog_name)['liked_posts']
 
         posts_list = []
         like_post_edges = []
 
         for post in posts:
             post_node = create_post_node(post)
-            posts.append(post_node)
-            like_post_edges.append(create_edge(blog_node.id, post_node.id))
+            posts_list.append(post_node)
+            like_post_edges.append(create_edge(blog_name, post_node['id']))
 
         dataframes = {
             "posts": pd.DataFrame(posts_list),
@@ -174,181 +198,9 @@ class Tumblr:
         posts_list = []
 
         for post in posts:
-            posts.append(create_post_node(post))
+            posts_list.append(create_post_node(post))
 
         dataframes = {
             "posts": pd.DataFrame(posts_list)
         }
         return dataframes
-
-
-"""def export_data(
-            self,
-            format,
-            object_type_list,
-            file_name,
-            options=None
-    ):
-        pass # TODO: export specified data to
-
-
-   class Blog (BareboneNode):
-    def __init__(
-            self,
-            title,
-            hostname,
-            num_posts,
-            last_time_updated,
-            description,
-            num_likes
-    ):
-        self.title = title
-        self.hostname = hostname
-        self.num_posts = num_posts
-        self.last_time_updated = last_time_updated
-        self.description = description
-        self.num_likes = num_likes
-
-
-class Post (BareboneNode):
-    def __init__(
-            self,
-            type,
-            timestamp,
-            date,
-            tags,
-            limit,
-            state,
-            liked,
-            num_posts,
-            creator
-    ):
-        self.type = type
-        self.timestamp = timestamp
-        self.date = date
-        self.tags = tags
-        self.limit = limit
-        self.state  = state
-        self.liked = liked
-        self.limit = limit
-        self.state = state
-        self.liked = liked
-        self.num_posts = num_posts
-        self.creator = creator
-        #TODO: add methods
-
-
-class TextPost(Post):
-    def __init__(
-            self,
-            type,
-            timestamp,
-            date,
-            tags,
-            limit,
-            state,
-            liked,
-            num_posts,
-            creator,
-            title,
-            body
-    ):
-        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
-        self.title = title
-        self.body = body
-
-
-class PhotoPost(Post):
-    def __init__(
-            self,
-            type,
-            timestamp,
-            date,
-            tags,
-            limit,
-            state,
-            liked,
-            num_posts,
-            creator,
-            photos,
-            caption,
-            width,
-            height
-    ):
-        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
-        self.photos = photos
-        self.caption = caption
-        self.width = width
-        self.height = height
-
-
-class QuotePost(Post):
-    def __init__(
-            self,
-            type,
-            timestamp,
-            date,
-            tags,
-            limit,
-            state,
-            liked,
-            num_posts,
-            creator,
-            text,
-            source
-    ):
-        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
-        self.text = text
-        self.source = source
-
-
-class LinkPost(Post):
-    def __init__(
-            self,
-            type,
-            timestamp,
-            date,
-            tags,
-            limit,
-            state,
-            liked,
-            num_posts,
-            creator,
-            title,
-            url,
-            author,
-            excerpt,
-            publisher,
-            photos,
-            description
-    ):
-        Post.__init__(self, type, timestamp, date, tags, limit, state, liked, num_posts, creator)
-        self.title = title
-        self.url = url
-        self.author = author
-        self.excerpt = excerpt
-        self.publisher = publisher
-        self.photos = photos
-        self.description = description
-
-
-# TODO: continue with chat, audio, video and answer posts
-
-
-# TODO: continue with edges
-class Like_post(BareboneEdge):
-    def __init__(
-            self,
-            user_id,
-            post_id
-    ):
-        BareboneEdge.__init__(self, user_id, post_id)
-
-
-class Follow_blog(BareboneEdge):
-    def __init__(
-            self,
-            following_blog_id,
-            followed_blog_id
-    ):
-        BareboneEdge.__init__(self, following_blog_id, followed_blog_id)"""""
