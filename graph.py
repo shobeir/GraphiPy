30d45e7ccb5f614c55e96bfeacf3550e39433c46
William Tjandra <wtjandra@usc.edu>
2018-10-14 16:17:34 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,31 +2,97 @@ import pandas as pd
 
 
 class Graph:
-    def __init__(self, nodes_dict={},  edges_dict={}):
-        self.nodes = nodes_dict
-        self.edges = edges_dict
+    def __init__(self, nodes_dict={},  edges_dict={}, option="pandas"):
+        self.option = option
+        self.node_count = 0
+        self.edge_count = 0
+        if option == "neo4j":
+            pass
+        elif option == "pandas":
+            self.nodes = nodes_dict
+            self.edges = edges_dict
+            self.nodes_df = {}
+            self.edges_df = {}
+        else:
+            self.nodes = nodes_dict
+            self.edges = edges_dict
 
     def create_node(self, node):
-        self.nodes[node.get_id()] = node
+        label = node.get_label_attribute()
+        _id = node.get_id()
+        if self.option == "neo4j":
+            pass
+        else:
+            if label not in self.nodes:
+                self.nodes[label] = {}
+                if self.option == "pandas":
+                    self.nodes_df[label] = pd.DataFrame(
+                        columns=vars(node).keys())
+            self.nodes[label][_id] = node
+
+        self.node_count += 1
+        if (self.node_count == 1000):
+            self.generate_df("node")
+
+    def generate_df(self, option):
+        if option == "node":
+            for key in self.nodes_df:
+                nodes_list = []
+                for _id in self.nodes[key]:
+                    nodes_list.append(vars(self.nodes[key][_id]))
+                df = pd.DataFrame(nodes_list)
+                self.nodes_df[key] = self.nodes_df[key].append(
+                    df, sort=False, ignore_index=True)
+                self.nodes[key] = {}
+            self.node_count = 0
+
+        else:
+            for key in self.edges_df:
+                edges_list = []
+                for _id in self.edges[key]:
+                    edges_list.append(vars(self.edges[key][_id]))
+                df = pd.DataFrame(edges_list)
+                self.edges_df[key] = self.edges_df[key].append(
+                    df, sort=False, ignore_index=True)
+                self.edges[key] = {}
+            self.edge_count = 0
 
     def create_edge(self, edge):
-        self.edges[edge.get_id()] = edge
+        label = edge.get_label_attribute()
+        _id = edge.get_id()
+        if self.option == "neo4j":
+            pass
+        else:
+            if label not in self.edges:
+                self.edges[label] = {}
+                if self.option == "pandas":
+                    self.edges_df[label] = pd.DataFrame(
+                        columns=vars(edge).keys())
+            self.edges[label][_id] = edge
+
+        self.edge_count += 1
+        if (self.edge_count == 2000):
+            self.generate_df("edge")
 
     def get_nodes(self):
+        if self.option == "pandas":
+            return self.nodes_df
         return self.nodes
 
     def get_edges(self):
+        if self.option == "pandas":
+            return self.edges_df
         return self.edges
 
     def get_dataFrame(self, dataFrame_type="pandas"):
         if dataFrame_type is "pandas":
             node_list = []
             edge_list = []
-            for id in self.nodes:
-                attributes = vars(self.nodes[id])
+            for _id in self.nodes:
+                attributes = vars(self.nodes[_id])
                 node_list.append(attributes)
-            for id in self.edges:
-                attributes = vars(self.edges[id])
+            for _id in self.edges:
+                attributes = vars(self.edges[_id])
                 edge_list.append(attributes)
 
             node_df = pd.DataFrame(node_list)
@@ -45,18 +111,27 @@ class Graph:
 
 
 class Node:
-    def __init__(self, _id, _label):
-        self.Id = _id
-        self.Label = _label
+    def __init__(self, _id, label, label_attribute):
+        self._id = _id
+        self.label = label
+        self.label_attribute = label_attribute
 
     def get_id(self):
-        return self.Id
+        return self._id
+
+    def get_label_attribute(self):
+        return self.label_attribute
 
 
 class Edge:
-    def __init__(self, _source, _target):
-        self.Source = _source
-        self.Target = _target
+    def __init__(self, source, target, label):
+        self.Source = source
+        self.Target = target
+        self.label = label
+        self.label_attribute = label
 
     def get_id(self):
         return self.Source + self.Target
+
+    def get_label_attribute(self):
+        return self.label_attribute
