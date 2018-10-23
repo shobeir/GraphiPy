fab75c35eebb4fcdf2f027aed037b840397fac32
William Tjandra <wtjandra@usc.edu>
2018-10-23 14:23:58 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-class Graph:
+class BaseGraph:
     def export_all_CSV(self, prefix):
         pass
 
@@ -18,25 +18,26 @@ class Graph:
         pass
 
 
-class Node:
+class BaseNode:
     def __init__(self, _id, label, label_attribute):
-        self.Id = _id
+        self._id = _id
         self.Label = label
         self.label_attribute = label_attribute
 
     def get_id(self):
-        return self.Id
+        return self._id
 
     def get_label_attribute(self):
         return self.label_attribute
 
 
-class Edge:
+class BaseEdge:
     def __init__(self, source, target, label):
         self.Source = source
         self.Target = target
-        self.label = label
+        self.Label = label
         self.label_attribute = label
+        self._id = ''.join([source, target, label])
 
     def get_id(self):
         return self.Source + self.Target
