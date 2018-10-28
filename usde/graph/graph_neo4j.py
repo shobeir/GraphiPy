553ebfed287fe55b0f5bce6236f526d8db34cf03
William Tjandra <wtjandra@usc.edu>
2018-10-27 20:42:28 -0700

    added comments for neo4j

--- usde/graph/graph_neo4j.py
+++ usde/graph/graph_neo4j.py
@@ -9,6 +9,7 @@ class NeoGraph(BaseGraph):
         self.graph = Graph(credentials)
 
     def export_all_CSV(self, prefix):
+        """ exports the whole graph as CSV file """
         query = "MATCH (n) RETURN n"
         table = self.graph.run(query).to_table()
         csv_file = open(prefix + ".csv", "w")
@@ -16,14 +17,22 @@ class NeoGraph(BaseGraph):
         csv_file.close()
 
     def export_CSV(self, prefix, node_option=set()):
+        """ exports selected nodes as separate CSV files """
         for key in node_option:
-            query = "MATCH (n:" + key + ") RETURN n"
+            query = "MATCH (n:" + key.lower() + ") RETURN n"
             csv_file = open(prefix + "_" + key + ".csv", "w")
             self.graph.run(query).to_table().write_csv(file=csv_file)
 
     def export_CSV_attr(self, prefix, node_option={}):
+        """
+        allows user to select specific attributes for each node 
+        node_option = {
+            "node_label": [attribute1, attribute2, attribute3, ...]
+        }
+        """
+
         for key in node_option:
-            query = ["MATCH (n:", key, ") RETURN "]
+            query = ["MATCH (n:", key.lower(), ") RETURN "]
             csv_file = open(prefix + "_" + key + "_node.csv", "w")
             if not node_option[key]:
                 query.append("n")
@@ -32,7 +41,7 @@ class NeoGraph(BaseGraph):
             else:
                 for attribute in node_option[key]:
                     query.append("n.")
-                    query.append(attribute)
+                    query.append(attribute.lower())
                     query.append(",")
                 query.pop()
                 query = ''.join(query)
@@ -40,11 +49,17 @@ class NeoGraph(BaseGraph):
             csv_file.close()
 
     def export_CSV_query(self, prefix, query, params):
-        table = self.graph.run(query, params=params).to_table()
+        """
+        Allows users to run their own query and exports
+        to CSV if applicable
+        """
+
+        table = self.graph.run(query, parameters=params).to_table()
         csv_file = open(prefix + ".csv", "w")
         table.write_csv(file=csv_file)
 
     def create_node(self, node):
+        """ Inserts a node into the graph """
         parameter_dict = {'params': vars(node)}
         query_list = [
             "MERGE (node: ",
@@ -57,6 +72,7 @@ class NeoGraph(BaseGraph):
         self.graph.run(query, parameters=parameter_dict)
 
     def create_edge(self, edge):
+        """ Creates a relationship between two nodes """
         source = edge.Source
         target = edge.Target
         parameter_dict = {'params': vars(edge)}
@@ -73,10 +89,13 @@ class NeoGraph(BaseGraph):
         self.graph.run(query, parameters=parameter_dict)
 
     def get_nodes(self):
+        """ returns a table of all the nodes """
         return self.graph.run("MATCH (n) RETURN n").to_table()
 
     def get_edges(self):
+        """ usde's NeoGraphh does not support querying edge """
         pass
 
     def execute(self, query, param={}):
+        """ Allows users to execute their own query """
         self.graph.run(query, parameters=param)
