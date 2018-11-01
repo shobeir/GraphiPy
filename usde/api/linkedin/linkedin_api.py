e701c1245ed25a9ab8903e171c2e357377ffe7fe
zifanshi <zifanshi@usc.edu>
2018-11-01 16:11:32 -0700

    linkdin api initial

--- usde/api/linkedin/linkedin_api.py
+++ usde/api/linkedin/linkedin_api.py
@@ -1,4 +1,18 @@
+import httplib2
+from usde.graph.graph_base import BaseNode as Node, BaseEdge as Edge, BaseGraph as Graph
+
+class UserNode(Node):
+    def __init__(self, user):
+        Node.__init__(self,user["id"], user["firstName"] + " " + user["lastName"], "user")
+        self.headline = user["headline"]
+
 class Linkedin:
-    def __init__(self,api):
+    def __init__(self,api, options="pandas"):
+        pass
+
+    def get_self_info(self):
+        url = "https://api.linkedin.com/v1/people/~?format=json"
+        http = httplib2.Http()
+        response = http.request(url, method="GET")
 
 
