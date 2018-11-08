4f899f0a91e8ae40266ad8eeca6e9b494a3e3728
Peixuan Liu <Olivia@MacBook-Pro-5.local>
2018-11-08 13:39:09 -0800

    added more attributes to tumblr posts

--- usde/api/tumblr/tumblr_api.py
+++ usde/api/tumblr/tumblr_api.py
@@ -28,6 +28,8 @@ class Tumblr:
         blog = self.tumblr.blog_info(blog_name)['blog']
         graph.create_node(Blog(blog))
 
+        return graph
+
     def fetch_blogs_following(
             self,
             graph,
@@ -58,6 +60,8 @@ class Tumblr:
         except KeyError:
             print(blogs_following_raw)
 
+        return graph
+
     def fetch_followers(
             self,
             graph,
@@ -86,6 +90,8 @@ class Tumblr:
                 graph.create_edge(Edge(blog['name'], follower['name'], "FOLLOWER"))
         except KeyError:
             print(followers_raw)
+
+        return graph
 
     def fetch_published_posts(
         self,
@@ -118,6 +124,8 @@ class Tumblr:
                 graph.create_edge(Edge(blog['name'], str(published_post['id']), "PUBLISHED"))
         except KeyError:
             print(published_posts_raw)
+
+        return graph
 
     def fetch_liked_posts(
         self,
@@ -152,6 +160,8 @@ class Tumblr:
         except KeyError:
             print(liked_posts_raw)
 
+        return graph
+
     def fetch_posts_tagged(
         self,
         graph,
@@ -178,6 +188,8 @@ class Tumblr:
             graph.create_node(Blog(blog))
             graph.create_edge(Edge(blog['name'], str(post_tagged['id']), "PUBLISHED"))
 
+        return graph
+
 
 class Blog (Node):
     """Tumblr's users are identified as Blogs"""
@@ -187,12 +199,12 @@ class Blog (Node):
         blog
     ):
         Node.__init__(self, blog['name'], blog['title'], "blog")
-        self.name = blog['name'],
-        self.title = blog['title'],
+        self.name = blog['name']
+        self.title = blog['title']
+        self.description = blog['description']
         self.url = blog['url']
         self.uuid = blog['uuid']
-        self.updated = blog['updated'],
-        self.description = blog['description']
+        self.updated = blog['updated']
 
 
 class Post (Node):
@@ -204,13 +216,107 @@ class Post (Node):
     ):
         # print(post)
         Node.__init__(self, post['id'], post['id'], "post")
-        self.blog_name = post['blog_name'],
-        self.post_url = post['post_url'],
-        self.type = post['type'],
-        self.timestamp = post['timestamp'],  # The time of the post, in seconds since the epoch
-        self.date = post['date'],  # The GMT date and time of the post, as a string
-        self.format = post['format'],  # String The post format: html or markdown
-        self.tags = post['tags'],  # Array(string) Tags applied to the post
-        self.state = post['state']
+        self.type = post['type']  # Any one from text, photo, quote, link, chat, audio
+        self.blog_name = post['blog_name']
+        self.post_url = post['post_url']
+        self.slug = post['slug']  # Short text summary to the end of the post URL
+        self.date = post['date']  # The GMT date and time of the post, as a string
+        self.timestamp = post['timestamp']  # The time of the post, in seconds since the epoch
+        self.state = post['state']   # Indicates the current state of the post
+        self.format = post['format']  # String The post format: html or markdown
+        self.reblog_key = post["reblog_key"]  # The key used to reblog this post
+        self.tags = post['tags']  # Array(string) Tags applied to the post
+        self.short_url = post['short_url']
+        self.summary = post['summary']
+        self.is_blocks_post_format = post['is_blocks_post_format']
+        self.recommended_source = post['recommended_source']
+        self.recommended_color = post['recommended_color']
+        self.followed = post['followed']
+        self.liked = post['liked']
+        self.note_count = post['note_count']
+        self.reblog = post['reblog']
+        self.trail = post['trail']
+        self.can_like = post['can_like']
+        self.can_reblog = post['can_reblog']
+        self.can_send_in_message = post['can_send_in_message']
+        self.can_reply = post['can_reply']
+        self.display_avatar = post['display_avatar']
+        self.title = None  # Special field for text, link, chat posts
+        self.body = None  # Special field for text, chat posts
+        self.caption = None  # Special field for photo, chat, video posts
+        self.image_permalink = None  # Special field for photo posts
+        self.photos = None  # Special field for photo, link posts
+        self.source_url = None  # Special field for quote, audio, video posts
+        self.source_title = None  # Special field for quote, audio, video posts
+        self.text = None  # Special field for quote posts
+        self.source = None  # Special field for quote posts
+        self.url = None  # Special field for link posts
+        self.link_author = None  # Special field for link posts
+        self.excerpt = None  # Special field for link posts
+        self.publisher = None  # Special field for link posts
+        self.description = None  # Special field for link posts
+        self.dialogue = None  # Special field for chat posts
+        self.id3_title = None  # Special field for audio posts
+        self.embed = None  # Special field for audio posts
+        self.plays = None  # Special field for audio posts
+        self.player = None  # Special field for video posts
+        self.asking_name = None  # Special field for answer posts
+        self.asking_url = None  # Special field for answer posts
+        self.question = None  # Special field for question posts
+        self.answer = None  # Special field for answer posts
+
+        try:
+            if "text" in self.type:
+                self.title = post['title']
+                self.body = post['body']
+            elif "photo" in self.type:
+                self.caption = post['caption']
+                self.image_permalink = post['image_permalink']
+                self.photos = post['photos']
+            elif "quote" in self.type:
+                print(post)
+                self.source_url = post['source_url']
+                self.source_title = post['source_title']
+                self.text = post['text']
+                self.source = post['source']
+            elif "link" in self.type:
+                self.title = post['title']
+                self.url = post['url']
+                self.link_author = post['link_author']
+                self.excerpt = post['excerpt']
+                self.publisher = post['publisher']
+                self.photos = post['photos']
+                self.description = post['description']
+            elif "chat" in self.type:
+                print(post)
+                self.title = post['title']
+                self.body = post['body']
+                self.dialogue = post['dialogue']
+                self.source_url = post['source_url']
+                self.source_title = post['source_title']
+                self.id3_title = post['id3_title']
+                self.caption = post['caption']
+                self.embed = post['embed']
+                self.plays = post['plays']
+            elif "video" in self.type:
+                print(post)
+                self.source_url = post['source_url']
+                self.source_title = post['source_title']
+                self.caption = post['caption']
+                self.player = post['player']
+            elif "answer" in self.type:
+                print(post)
+                self.asking_name = post['asking_name']
+                self.asking_url = post['asking_name']
+                self.question = post['question']
+                self.answer = post['answer']
+        except KeyError:
+            print("KeyError")
+            print(post)
+
+
+
+
+
 
 
