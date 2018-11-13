a73ce4a8405faef249a216c9eb20bdb35c66df4d
William Tjandra <wtjandra@usc.edu>
2018-11-12 22:12:50 -0800

    Added function to draw

--- graphipy/graphipy.py
+++ graphipy/graphipy.py
@@ -10,6 +10,12 @@ from graphipy.graph.graph_pandas import PandasGraph
 from graphipy.graph.graph_dict import DictGraph
 from graphipy.graph.graph_neo4j import NeoGraph
 
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib import colors as c
+import random
+import os
+
 
 class GraphiPy:
     def __init__(self, option="pandas"):
@@ -54,12 +60,17 @@ class GraphiPy:
 
         return PandasGraph()
 
-    def nx_create_from_pd(self, nx_graph, edges_df, nodes_df, options):
+    def nx_create_from_pd(self, nodes_df, edges_df, nx_graph=None, directional=False):
         # Create graph from edgelist dataframes
-        for key in edges_df:
+        if nx_graph is None:
+            if directional:
+                nx_graph = nx.DiGraph()
+            else:
+                nx_graph = nx.Graph()
 
+        for key in edges_df:
             new_graph = nx.from_pandas_edgelist(
-                edges_df[key], source=options["Source"], target=options["Target"], edge_attr=True)
+                edges_df[key], source="Source", target="Target", edge_attr=True)
 
             nx_graph = nx.compose(nx_graph, new_graph)
 
@@ -74,7 +85,15 @@ class GraphiPy:
                 for attr in row.keys():
                     node[attr] = row[attr]
 
-    def nx_create_from_dict(self, nx_graph, edges_dict, nodes_dict, options):
+        return nx_graph
+
+    def nx_create_from_dict(self, edges_dict, nodes_dict, nx_graph=None, directional=False):
+        # Create graph from edge dictionaries
+        if nx_graph is None:
+            if directional:
+                nx_graph = nx.DiGraph()
+            else:
+                nx_graph = nx.Graph()
 
         for key in edges_dict:
             edges = edges_dict[key]
@@ -89,6 +108,7 @@ class GraphiPy:
                 }
                 nx_graph.add_edge(source, target, attr_dict=attr)
 
+        # Add node attributes
         for key in nodes_dict:
             nodes = nodes_dict[key]
 
@@ -98,3 +118,76 @@ class GraphiPy:
 
                 for attr in node.keys():
                     nx_node[attr] = node[attr]
+
+        return nx_graph
+
+    def nx_create_from_csv():
+        pass
+
+    def nx_create_from_neo4j():
+        pass
+
+    def nx_draw_random(self, nx_graph=None, pos=None, legend=False, color_edge=True, axis=False):
+        import matplotlib.colors as c
+        import random
+
+        if pos is None:
+            pos = nx.random_layout(nx_graph)
+
+        if not axis:
+            plt.axis('off')
+
+        # Get labels
+        node_labels = nx.get_node_attributes(nx_graph, "Label")
+        edge_labels = nx.get_edge_attributes(nx_graph, "Label")
+
+        # Separate nodes by category
+        node_categories = {}
+        for node in nx_graph.nodes(data=True):
+            node = node[1]
+            key = node["Label"]
+            if key in node_categories:
+                node_categories[key].append(node["_id"])
+            else:
+                node_categories[key] = [node["_id"]]
+
+        # Separate edges by category
+        edge_categories = {}
+        for edge in nx_graph.edges(data=True):
+            key = edge[2]["Label"]
+            if key in edge_categories:
+                edge_categories[key].append((edge[0], edge[1]))
+            else:
+                edge_categories[key] = [(edge[0], edge[1])]
+
+        # Draw nodes
+        colors = set(c.BASE_COLORS)
+        colors.remove("w")
+        chosen = set()
+        draw_nodes = []
+        for key in node_categories:
+            color = random.choice(tuple(colors))
+            draw_nodes.append(nx.draw_networkx_nodes(
+                node_categories[key], pos, node_color=color, node_size=75, label=key))
+            colors.remove(color)
+
+        # Draw edges
+        if color_edge:
+            colors = set(c.BASE_COLORS)
+            colors.remove("w")
+        draw_edges = []
+        for key in edge_categories:
+            if color_edge:
+                color = random.choice(tuple(colors))
+            else:
+                color = "b"
+            draw_edges.append(nx.draw_networkx_edges(
+                nx_graph, edgelist=edge_categories[key], pos=pos, edge_color=color, label=key, width=2))
+            colors.remove(color)
+
+        if legend:
+            ncol = len(node_categories) if len(node_categories) > len(
+                edge_categories) else len(edge_categories)
+            if ncol > 4:
+                ncol = 4
+            return plt.legend(loc=9, bbox_to_anchor=(0.5, -0.1), ncol=ncol)
